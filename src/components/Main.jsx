83c8ac1485a21cad6873d316b94f692c481b6f66
frank-Mwangi <kungofrank@gmail.com>
2024-01-30 01:03:19 +0300

    Profile page all contents loaded

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "../styles/main.scss";
 import CompleteProfile from "./CompleteProfile";
+import Intro from "./Intro";
+import NewPost from "./NewPost";
+import Post from "./Post";
 
 const Main = () => {
   return (
     <div className="main">
       <div className="profile-info">
         <CompleteProfile />
+        <Intro />
       </div>
-      <div className="timeline"></div>
+      <div className="timeline">
+        <NewPost />
+        <Post />
+      </div>
     </div>
   );
 };
