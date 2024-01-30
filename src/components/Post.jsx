f53835457a773268743c2fa79466f2a01e719e0b
frank-Mwangi <kungofrank@gmail.com>
2024-01-30 12:14:42 +0300

    Timeline done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,55 +7,57 @@ import comment from "../assets/message-circle.png";
 import share from "../assets/share.png";
 import emoji from "../assets/mood-smile.png";
 import link from "../assets/link.png";
+import "../styles/post.scss";
 
 const Post = () => {
   return (
-    <div>
+    <div className="post">
       <div className="handle">
         <img src={avi} alt="no-avi" />
         <div className="name">
           <h4>Kaiser Soze</h4>
           <p>56 mins ago</p>
         </div>
-        <div className="textContent">
-          <p>
-            Here are some photography works that I made on the weekend with some
-            of my friends, happy for that!
-          </p>
-        </div>
-        <div className="images">
-          <img src={photo1} alt="no-pic" />
-          <img src={photo2} alt="no-pic" />
-        </div>
-        <div className="impressions">
-          <div className="impression">
-            <img src={heart} alt="like-icon" />
-            <p>2.6K Likes</p>
-          </div>
-          <div className="impression">
-            <img src={comment} alt="comment-icon" />
-            <p>297 Comments</p>
-          </div>
-          <div className="impression">
-            <img src={share} alt="share-icon" />
-            <p>201 Share</p>
-          </div>
-        </div>
-        <form className="comment">
-          <input
-            type="text"
-            name="comment"
-            id="comment"
-            className="write-comment"
-          />
-          <button>
-            <img src={emoji} alt="" />
-          </button>
-          <button>
-            <img src={link} alt="" />
-          </button>
-        </form>
       </div>
+      <div className="textContent">
+        <p>
+          Here are some photography works that I made on the weekend with some
+          of my friends, happy for that!
+        </p>
+      </div>
+      <div className="images">
+        <img src={photo1} alt="no-pic" />
+        <img src={photo2} alt="no-pic" />
+      </div>
+      <div className="impressions">
+        <div className="impression">
+          <img src={heart} alt="like-icon" />
+          <p>2.6K Likes</p>
+        </div>
+        <div className="impression">
+          <img src={comment} alt="comment-icon" />
+          <p>297 Comments</p>
+        </div>
+        <div className="impression">
+          <img src={share} alt="share-icon" />
+          <p>201 Share</p>
+        </div>
+      </div>
+      <form className="comment">
+        <input
+          type="text"
+          name="comment"
+          id="comment"
+          className="write-comment"
+          placeholder="Write your message ..."
+        />
+        <button>
+          <img src={emoji} alt="" />
+        </button>
+        <button>
+          <img src={link} alt="" />
+        </button>
+      </form>
     </div>
   );
 };
