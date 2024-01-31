61a2ad2f1936c4e26ca666e1ed906f766683397e
frank-Mwangi <kungofrank@gmail.com>
2024-01-31 20:51:42 +0300

    Timeline scaffold added and routed

---
+++ src/pages/Friends.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import avi from "../assets/Avatar.png";
+
+const Friends = () => {
+  const friends = [
+    {
+      name: "Angela Lee",
+      handle: "anglee",
+      dp: avi,
+    },
+  ];
+  return <div>Friends</div>;
+};
+
+export default Friends;
