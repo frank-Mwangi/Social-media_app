37e785c2d86be9b1722f89740e0db34a9377579f
frank-Mwangi <kungofrank@gmail.com>
2024-01-31 15:11:22 +0300

    Restructured sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,54 +12,92 @@ import mobileIcon from "../assets/mobiledesign.png";
 import productIcon from "../assets/productdesign.png";
 
 const Sidebar = () => {
-  const menuItems = [
-    {
-      id: 0,
-      icon: timelineIcon,
-      title: "Timeline",
-    },
-    {
-      id: 1,
-      icon: friendsIcon,
-      title: "Friends",
-    },
-    {
-      id: 2,
-      icon: groupsIcon,
-      title: "Groups",
-    },
-    {
-      id: 3,
-      icon: videosIcon,
-      title: "Videos",
-    },
-    {
-      id: 4,
-      icon: photosIcon,
-      title: "Photos",
-    },
-    {
-      id: 5,
-      icon: eventsIcon,
-      title: "Events",
-      value: 10,
-    },
-  ];
+  // const menuItems = [
+  //   {
+  //     id: 0,
+  //     icon: timelineIcon,
+  //     title: "Timeline",
+  //   },
+  //   {
+  //     id: 1,
+  //     icon: friendsIcon,
+  //     title: "Friends",
+  //   },
+  //   {
+  //     id: 2,
+  //     icon: groupsIcon,
+  //     title: "Groups",
+  //   },
+  //   {
+  //     id: 3,
+  //     icon: videosIcon,
+  //     title: "Videos",
+  //   },
+  //   {
+  //     id: 4,
+  //     icon: photosIcon,
+  //     title: "Photos",
+  //   },
+  //   {
+  //     id: 5,
+  //     icon: eventsIcon,
+  //     title: "Events",
+  //     value: 10,
+  //   },
+  // ];
 
-  const shortcuts = [
-    {
-      icon: websiteIcon,
-      title: "Website Design",
-    },
-    {
-      icon: mobileIcon,
-      title: "Mobile Design",
-    },
-    {
-      icon: productIcon,
-      title: "Product Design",
-    },
-  ];
+  // return (
+  //   <div className="sidebar">
+  //     <div className="avatar">
+  //       <img src={avi} alt="no=avi" />
+  //       <div className="details">
+  //         <h4>Kaizer Soze</h4>
+  //         <p>@K.soze</p>
+  //       </div>
+  //     </div>
+  //     <div className="menu">
+  //       <h2>MENU</h2>
+  //       {menuItems &&
+  //         menuItems.map((item, index) => {
+  //           return (
+  //             <div className="menuitem" key={index}>
+  //               <div className="menuitemleft">
+  //                 <img src={item.icon} alt={item.title} />
+  //                 {item.value ? (
+  //                   <span style={{ color: "#ED4F9D" }}> {item.title}</span>
+  //                 ) : (
+  //                   <span className="noNotifStyle">{item.title}</span>
+  //                 )}
+  //               </div>
+  //               <div className="menuItemRight">
+  //                 {item.value ? (
+  //                   <>
+  //                     {item.value && (
+  //                       <span
+  //                         style={{
+  //                           color: "white",
+  //                           backgroundColor: "#ED4F9D",
+  //                           borderRadius: "50%",
+  //                           padding: "3px 5px",
+  //                         }}
+  //                       >
+  //                         {item.value}
+  //                       </span>
+  //                     )}
+  //                   </>
+  //                 ) : (
+  //                   <>
+  //                     {item.value && (
+  //                       <span className="value">{item.value}</span>
+  //                     )}
+  //                   </>
+  //                 )}
+  //               </div>
+  //             </div>
+  //           );
+  //         })}
+  //     </div>
+  //     {/* <Menu /> */}
 
   return (
     <div className="sidebar">
@@ -72,61 +110,81 @@ const Sidebar = () => {
       </div>
       <div className="menu">
         <h2>MENU</h2>
-        {menuItems &&
-          menuItems.map((item, index) => {
-            return (
-              <div className="menuitem" key={index}>
-                <div className="menuitemleft">
-                  <img src={item.icon} alt={item.title} />
-                  {item.value ? (
-                    <span style={{ color: "#ED4F9D" }}> {item.title}</span>
-                  ) : (
-                    <span className="noNotifStyle">{item.title}</span>
-                  )}
-                </div>
-                <div className="menuItemRight">
-                  {item.value ? (
-                    <>
-                      {item.value && (
-                        <span
-                          style={{
-                            color: "white",
-                            backgroundColor: "#ED4F9D",
-                            borderRadius: "50%",
-                            padding: "3px 5px",
-                          }}
-                        >
-                          {item.value}
-                        </span>
-                      )}
-                    </>
-                  ) : (
-                    <>
-                      {item.value && (
-                        <span className="value">{item.value}</span>
-                      )}
-                    </>
-                  )}
-                </div>
-              </div>
-            );
-          })}
+        <div className="menuitem">
+          <div className="menuitemleft">
+            <img src={timelineIcon} alt="Timeline" />
+            <span>Timeline</span>
+          </div>
+          <div className="menuItemRight"></div>
+        </div>
+
+        <div className="menuitem">
+          <div className="menuitemleft">
+            <img src={friendsIcon} alt="Friends" />
+            <span>Friends</span>
+          </div>
+          <div className="menuItemRight"></div>
+        </div>
+
+        <div className="menuitem">
+          <div className="menuitemleft">
+            <img src={groupsIcon} alt="Groups" />
+            <span>Groups</span>
+          </div>
+          <div className="menuItemRight"></div>
+        </div>
+
+        <div className="menuitem">
+          <div className="menuitemleft">
+            <img src={videosIcon} alt="Videos" />
+            <span>Videos</span>
+          </div>
+          <div className="menuItemRight"></div>
+        </div>
+
+        <div className="menuitem">
+          <div className="menuitemleft">
+            <img src={photosIcon} alt="Photos" />
+            <span>Photos</span>
+          </div>
+          <div className="menuItemRight"></div>
+        </div>
+
+        <div className="menuitem">
+          <div className="menuitemleft">
+            <img src={eventsIcon} alt="Events" />
+            <span style={{ color: "#ED4F9D" }}> Events</span>
+          </div>
+          <div className="menuItemRight">
+            <span
+              style={{
+                color: "white",
+                backgroundColor: "#ED4F9D",
+                borderRadius: "50%",
+                padding: "3px 5px",
+              }}
+            >
+              10
+            </span>
+          </div>
+        </div>
       </div>
-      {/* <Menu /> */}
       <div className="shortcuts">
         <h2>SHORTCUTS</h2>
-        {shortcuts &&
-          shortcuts.map((item, index) => {
-            return (
-              <div className="shortcut" key={index}>
-                <img src={item.icon} alt="no-icon" />
-                {item.title}
-              </div>
-            );
-          })}
+        <div className="shortcut">
+          <img src={websiteIcon} alt="no-icon" />
+          <p>Website Design</p>
+        </div>
+        <div className="shortcut">
+          <img src={mobileIcon} alt="no-icon" />
+          <p>Mobile Design</p>
+        </div>
+        <div className="shortcut">
+          <img src={productIcon} alt="no-icon" />
+          <p>Product Design</p>
+        </div>
       </div>
     </div>
   );
 };
-
 export default Sidebar;
